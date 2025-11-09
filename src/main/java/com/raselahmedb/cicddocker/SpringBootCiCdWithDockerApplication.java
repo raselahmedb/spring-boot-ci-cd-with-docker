e31bd473853a8fc2d93f2642d0c59cd620bee291
Rasel Ahmed <raselahmedb@gmail.com>
2025-11-09 17:00:52 +0600

    first commit

---
+++ src/main/java/com/raselahmedb/cicddocker/SpringBootCiCdWithDockerApplication.java
@@ -0,0 +1,13 @@
+package com.raselahmedb.cicddocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCiCdWithDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCiCdWithDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raselahmedb/cicddocker/SpringBootCiCdWithDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raselahmedb.cicddocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCiCdWithDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
